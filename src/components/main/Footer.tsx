6f2f640e5000c6c1828955f449840d4f5a672176
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-09-30 22:38:27 -0400

    adding static.json

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
+// import { Box, Container, Row, Column, FooterLink, Heading } from './FooterStyles.css';
 import '../centerSection/Center.css'
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 export class Footer extends React.Component {
     render() {
         return (
-            <div>
-                GitHub, LinkedIn, Issues, Api Resources, SuperHeroStuff.com
-            </div>
+            <div></div>
         )
+        //     <Box>
+        //     <Container>
+        //         <Row>
+        //             <Column>
+        //                 <Heading>Creators</Heading>
+        //                 <FooterLink href="https://www.linkedin.com/in/abel-watson/" target="_blank">
+        //                     {/* <LinkedInIcon />  */}
+        //                     Abel Watson</FooterLink>
+        //             </Column>
+        //             <Column>
+        //                 <Heading>Disclaimer</Heading>
+        //                 <FooterLink style={{ fontSize: 12 }} href="https://creativecommons.org/licenses/by-sa/3.0/" target="_blank" > Unless otherwise stated, the content of this page is licensed under Creative Commons Attribution-ShareAlike 3.0 License </FooterLink>
+        //             </Column>
+        //             <Column>
+        //                 <Heading>Resources</Heading>
+        //                 <FooterLink href="http://dnd5e.wikidot.com/" target="_blank"> DnD 5e WikiDot </FooterLink>
+        //                 <FooterLink href="http://rolladvantage.com/tokenstamp/" target="_blank"> Token Creator </FooterLink>
+        //                 <FooterLink href="https://app.roll20.net/" target="_blank"> Roll20 Online Play </FooterLink>
+        //             </Column>
+        //             <Column>
+        //                 <Heading>Resources</Heading>
+        //                 <FooterLink href="https://dnd.wizards.com/" target="_blank">Dungeons &amp; Dragons</FooterLink>
+        //                 <FooterLink href="https://dicegrimorium.com/free-rpg-map-library/" target="_blank"> Free Digital Maps </FooterLink>
+        //                 <FooterLink href="https://www.dndbeyond.com/" target="_blank"> D&amp;D Beyond </FooterLink>
+        //             </Column>
+        //             <Column>
+        //                 <Heading>Issues</Heading>
+        //                 <FooterLink href="https://github.com/BHanna2021/xmendndclient/issues" target="_blank">
+        //                     {/* <GitHubIcon />  */}
+        //                     GitHub Repository</FooterLink>
+        //             </Column>
+        //         </Row>
+        //     </Container>
+        // </Box>
+        // )
     }
 }
 
